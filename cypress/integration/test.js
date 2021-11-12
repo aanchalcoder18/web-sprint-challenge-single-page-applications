8d8bd2ad9474ac14ed2216ead4271ec8a2ebd796
aanchalcoder18 <aasaxena14@gmail.com>
2021-11-12 08:54:44 -0800

    updates

---
+++ cypress/integration/test.js
@@ -0,0 +1,12 @@
+// - [ ] test that you can add text to the box
+// - [ ] test that you can select multiple toppings
+// - [ ] test that you can submit the form
+
+describe('Form App Testing', () => {
+
+
+    beforeEach(() => {
+        cy.visit('http://localhost:3000/')
+    })
+
+})
